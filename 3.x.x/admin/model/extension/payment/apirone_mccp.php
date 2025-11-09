93f9825931c892358f13c2e9ed729394bf7b1999
Valery Yu <vvy1976@gmail.com>
2025-11-09 19:17:21 +0400

    restructure files and folders for all OC versions; fix coins CSS for different container width

--- 3.x.x/admin/model/extension/payment/apirone_mccp.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(DIR_SYSTEM . 'library/apirone/model/admin/apirone_mccp.php');
-
-// the class name formation matters
-class ModelExtensionPaymentApironeMccp extends \Apirone\Payment\Model\Admin\ModelExtensionPaymentApironeMccpAdmin {}
--- apirone/controller/common/apirone_mccp.php
+++ apirone/controller/common/apirone_mccp.php
@@ -3,7 +3,7 @@
 namespace Apirone\Payment\Controller;
 
 require_once(((int) explode('.', VERSION, 2)[0] < 4 ? DIR_SYSTEM . 'library/apirone/' : DIR_EXTENSION . 'apirone/system/library/') . 'apirone_mccp.php');
-require_once(PATH_TO_LIBRARY . 'controller/common' . (OC_MAJOR_VERSION < 4 ? '_before_oc4' : '') . '.php');
+require_once(PATH_TO_LIBRARY . 'controller/common_' . (OC_MAJOR_VERSION < 4 ? 'before' : 'since') . '_v4.php');
 require_once(PATH_TO_LIBRARY . 'vendor/autoload.php');
 
 use \Apirone\SDK\Model\Settings;
